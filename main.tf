8d8ad161454032e61f8c0a5c7089007f5ad77346
gani321 <90252677+gani321@users.noreply.github.com>
2022-01-31 15:31:23 +0530

    1sst

--- main.tf
+++ main.tf
@@ -1,25 +1,16 @@
 provider "aws" {
-  region     = "us-west-2"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  region     = "ap-south-1"
+  access_key = "AKIAXNVYN5UADEI7AC4C"
+  secret_key = "OVDom2pQRaPfW88ufNQYGCVl"
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+resource "aws_vpc" "vpc" {
+  cidr_block       = 10.0.0.0/16
+  instance_tenancy = "default"
+  enable_dns_support = "true"
 
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    Name = defaut 
+
   }
 }
